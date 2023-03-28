193bc0deade593c69bb24fe886306da2f8af2e80
aadimr <adityashaw9066@gmail.com>
2023-03-28 12:29:42 +0530

    done

---
+++ 28no.LeetCode.js
@@ -0,0 +1,9 @@
+var strStr = function(haystack, needle) {
+   return haystack.indexOf(needle)
+};
+
+// testCases below:-
+
+// console.log(strStr("leetcode","leeto"))
+// console.log(strStr("sadbutsad","sad"))
+// console.log(strStr("aditya","it"))
